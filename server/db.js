19f0076373bd89f2962f0eba7254f372a424f9e9
Benjamin Pak <benjaminjpak95@gmail.com>
2023-06-01 11:43:56 -0700

    updated env file to match ec2 instances

--- server/db.js
+++ server/db.js
@@ -6,7 +6,8 @@ const pool = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT
+  port: process.env.DB_PORT,
+  max: 20,
 });
 // pool.connect()
 //   .then(() => console.log('Successfully connected to the database!'))
